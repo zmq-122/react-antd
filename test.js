77c444588826ba201a48190ccd21148f72cd68bb
alana <1440863767@qq.com>
2022-06-07 14:19:25 +0800

    test2

--- test.js
+++ test.js
@@ -2,3 +2,10 @@ console.log("test")
 console.log("async edit")
 console.log("net github edit test")
 console.log("test2 github")
+console.log("loacal test 2")
+// pull add commit push
+// git merge --abort
+
+// git reset --merge
+
+// git pull
